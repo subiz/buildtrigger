0f907f55c419b7a0dc8d6e780f1f6f544cdfc7e8
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-10-15 12:16:23 +0700

    fix dockerbuild

--- gcp.js
+++ gcp.js
@@ -74,7 +74,7 @@ const gSteps = [
 		entrypoint: 'sh',
 		args: [
 			'-c',
-			'[ -f Dockerfile ] && cp Dockerfile /tmp/$_NAME.Dockerfile && ./.configmap -config=.config.yaml -format=docker -compact configmap.yaml >> /tmp/$_NAME.Dockerfile && docker build -q -t $_DOCKERHOST$_ORG/$_NAME:$_VERSION -f /tmp/$_NAME.Dockerfile . && docker push ${_DOCKERHOST}$_ORG/$_NAME:$_VERSION',
+			'[ -f Dockerfile ] && cp Dockerfile .$_NAME.Dockerfile.tmp && ./.configmap -config=.config.yaml -format=docker -compact configmap.yaml >> .$_NAME.Dockerfile.tmp && docker build -q -t $_DOCKERHOST$_ORG/$_NAME:$_VERSION -f .$_NAME.Dockerfile.tmp . && docker push ${_DOCKERHOST}$_ORG/$_NAME:$_VERSION',
 		],
 		waitFor: ['run'],
 	},
