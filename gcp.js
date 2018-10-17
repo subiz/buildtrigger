0c4c3bd164777fa165b02cbd878eafb0fe82f9af
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-10-17 22:31:48 +0700

    cat build file

--- gcp.js
+++ gcp.js
@@ -68,7 +68,7 @@ args: ['info'],
 		],
 		args: [
 			'-c',
-			'cd ~ && pwd && echo "#!/bin/sh" > /tmp/$_NAME.build && ./.dockerun build.yaml >> /tmp/$_NAME.build && chmod +x /tmp/$_NAME.build && /tmp/$_NAME.build',
+			'echo "#!/bin/sh" > /tmp/$_NAME.build && ./.dockerun build.yaml >> /tmp/$_NAME.build && chmod +x /tmp/$_NAME.build && cat /tmp/$_NAME.build && /tmp/$_NAME.build',
 		],
 		waitFor: ['cache', 'git'],
 	},
