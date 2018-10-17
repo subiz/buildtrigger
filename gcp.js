1ea942b272afb4f66d2d850fc58eed55a5629c54
Phạm Kiều Thanh <hhnnaahhtt@gmail.com>
2018-10-17 23:16:43 +0700

    move house to current directory

--- gcp.js
+++ gcp.js
@@ -68,7 +68,7 @@ args: ['info'],
 		],
 		args: [
 			'-c',
-			'cp -R ~ /house && ls -lahR /house && echo "#!/bin/sh" > /tmp/$_NAME.build && ./.dockerun build.yaml >> /tmp/$_NAME.build && chmod +x /tmp/$_NAME.build && cat /tmp/$_NAME.build && /tmp/$_NAME.build',
+			'cp -R ~ ./house && ls -lahR ./house && echo "#!/bin/sh" > /tmp/$_NAME.build && ./.dockerun build.yaml >> /tmp/$_NAME.build && chmod +x /tmp/$_NAME.build && cat /tmp/$_NAME.build && /tmp/$_NAME.build',
 		],
 		waitFor: ['cache', 'git'],
 	},
